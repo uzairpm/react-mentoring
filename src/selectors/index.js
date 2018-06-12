2d9059d02c3b71cd639e50bf2c554404d259f9eb
Uzair Ahmed <uzairpm@gmail.com>
2018-06-12 19:14:35 +0200

    Bug fix

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -3,6 +3,6 @@ export const getTitleActiveState = (state) => state.titleActive;
 export const getSortByReleaseDateState = (state) => state.sortByReleaseDate;
 export const getSearchtext = (state) => state.searchText;
 
-export const getMovie = (state) => state.movie;
-export const getSelectedGenre = (state) => state.selectedGenre;
-export const getSimilarMovies = (state) => state.similarMovies;
+export const getMovie = (state) => state.movie || {};
+export const getSelectedGenre = (state) => state.selectedGenre || '';
+export const getSimilarMovies = (state) => state.similarMovies || [];
